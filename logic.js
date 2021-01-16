f1e8ee9c3e050170f5689b075b2a39a4ad75c641
Smogo <aurelien.genois@gmail.com>
2021-01-16 11:40:09 +0100

    rewrite + clean js

--- logic.js
+++ logic.js
@@ -1,22 +1,10 @@
-// function Book(title, author, nbPages) {
-//     this.title = title;
-//     this.author = author;
-//     this.nbPages = nbPages;
-//     this.read = false;
-//     // this.info = function() {
-//     //     return `${this.title} by ${this.author}, ${this.nbPages} pages, ${this.read ? 'read' : 'not read yet'}`;
-//     // }
-// }
-// // If we declare the function directly in the constructor, that function would be duplicated every time a new Book is created
-// Book.prototype.info = function() {
-//     return `${this.title} by ${this.author}, ${this.nbPages} pages, ${this.read ? 'read' : 'not read yet'}`;
-//     }
-
-// const theHobbit = new Book("The Hobbit", "J. R. R. Tolkien", 295);
-
-/////////////////////////////////////////////////////////////////////////////:
+const newBookButton = document.querySelector('#new-book');
+const addBookForm = document.querySelector('#add-book-form');
+const booksList = document.querySelector('#books-list');
+let myLibrary = JSON.parse(localStorage.getItem('books')) || [];
 
 // Book constructor
+
 function Book(title, author, nbPages) {
     this.title = title;
     this.author = author;
@@ -27,19 +15,56 @@ Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.nbPages} pages, ${this.read ? 'read' : 'not read yet'}`;
 }
 Book.prototype.toggleRead = function() {
-    this.read = this.read ? false : true;
+    this.read = !this.read;
+    //this.read = this.read ? false : true;
 }
 
-const addBookForm = document.querySelector('#add-book-form');
-const booksList = document.querySelector('#books-list');
-let myLibrary = JSON.parse(localStorage.getItem('books')) || [];
+// Library
+
+// when the book is in the local storage, it loosed is prototype, so its methods too
 myLibrary.map(book => {
     // TEMP add Book.prototype to the objects from the localStorage
     book.__proto__ = Book.prototype;
-})
+});
 
-displayBooks(myLibrary);
-console.log(myLibrary);
+
+
+function createBookItem(title, author, nbPages, read, bookIndex) {
+    const li = document.createElement('li');
+    li.setAttribute("data-book-index",bookIndex);
+    const bookTitleText = document.createElement('p');
+    bookTitleText.textContent = title;
+    const bookAuthorText = document.createElement('p');
+    bookAuthorText.textContent = author;
+    const bookNbPagesText = document.createElement('p');
+    bookNbPagesText.textContent = nbPages;
+    const button = document.createElement('button');
+    button.setAttribute('data-action', "change-status");
+    button.textContent = read ? 'read' : 'unread';
+    const removeButton = document.createElement('button');
+    removeButton.setAttribute('data-action', "remove");
+    removeButton.textContent = "Remove";
+
+    li.appendChild(bookTitleText);
+    li.appendChild(bookAuthorText);
+    li.appendChild(bookNbPagesText);
+    li.appendChild(button);
+    li.appendChild(removeButton);
+    
+    return li;
+}
+
+// NewBook Form, remove and change
+
+function displayBooks(library) {
+    while (booksList.firstChild) {    
+        booksList.removeChild(booksList.firstChild);
+    }
+    library.forEach((book,bookIndex) => {
+        const bookItem = createBookItem(book.title, book.author, book.nbPages, book.read, bookIndex);
+        booksList.appendChild(bookItem); 
+    });
+}
 
 function addBookToLibrary(title, author, nbPages, read) {
     const newBook = new Book(title, author, nbPages);
@@ -48,43 +73,20 @@ function addBookToLibrary(title, author, nbPages, read) {
     localStorage.setItem('books', JSON.stringify(myLibrary));
 }
 
-function displayBooks(library) {
-    while (booksList.firstChild) {    
-        booksList.removeChild(booksList.firstChild);
-    }
-    library.map(book => {
-        // create a list item and add them (=> to group in its own function)
-        const li = document.createElement('li');
-        const bookTitleText = document.createElement('p');
-        bookTitleText.textContent = book.title;
-        const bookAuthorText = document.createElement('p');
-        bookAuthorText.textContent = book.author;
-        const bookNbPagesText = document.createElement('p');
-        bookNbPagesText.textContent = book.nbPages;
-        // const bookReadText = document.createElement('p');
-        // bookReadText.textContent = "is read: " + book.read;
-        const button = document.createElement('button');
-        button.textContent = book.read ? 'read' : 'unread';
-        
-        button.addEventListener('click', function() {
-            console.log(book);
-            book.toggleRead();
-            this.textContent = book.read ? 'read' : 'unread';
-            localStorage.setItem('books', JSON.stringify(myLibrary));
-        })
-        // add the list item to the ul
-        li.appendChild(bookTitleText);
-        li.appendChild(bookAuthorText);
-        li.appendChild(bookNbPagesText);
-        // li.appendChild(bookReadText);
-        li.appendChild(button);
-
-        booksList.appendChild(li);    
-    });
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    displayBooks(myLibrary);
+    localStorage.setItem('books', JSON.stringify(myLibrary));
 }
 
+function toggleReadStatus(index) {
+    myLibrary[index].toggleRead();
+    textContent = myLibrary[index].read ? 'read' : 'unread';
+    displayBooks(myLibrary);
+    localStorage.setItem('books', JSON.stringify(myLibrary));
+}
 
-addBookForm.addEventListener('submit', (e) => {
+function manageNewBookSubmit(e) {
     e.preventDefault();
     const bookTitle = addBookForm.querySelector('#book-title').value;
     const bookAuthor = addBookForm.querySelector('#book-author').value;
@@ -94,8 +96,24 @@ addBookForm.addEventListener('submit', (e) => {
 
     addBookToLibrary(bookTitle, bookAuthor, bookNbPages, bookRead);
     displayBooks(myLibrary);
-
+    // reset the inputs
     addBookForm.querySelectorAll('.add-input').forEach(input => input.value = "");
     addBookForm.querySelector('[type="checkbox"]').checked = false;
-})
+    addBookForm.classList.remove('display');
+}
 
+
+newBookButton.addEventListener('click', () => addBookForm.classList.add('display'));
+addBookForm.addEventListener('submit', manageNewBookSubmit);
+booksList.addEventListener('click', (e) => {
+    const bookIndex = e.target.parentNode.dataset.bookIndex;
+    switch(e.target.dataset.action) {
+        case "remove": 
+            removeBook(bookIndex);
+            break;
+        case "change-status":
+            toggleReadStatus(bookIndex);
+        }
+    });
+
+displayBooks(myLibrary)
